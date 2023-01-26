87262e0d842f209980b0191a0a4bcf157aeb4141
Omkar141998 <120208397+Omkar141998@users.noreply.github.com>
2023-01-26 09:09:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // complete the given function
 
 function palindrome(str){
+	const alphanumericOnly = str.toLowerCase().match(/[a-z0-9]/g);
+// non alpha numric character
+	return aplphanumricOnly.join("")=== alphanumricOnly.reverse().join("")
 
 }
+coonsole.log(palindrome("racecar"));
 module.exports = palindrome
