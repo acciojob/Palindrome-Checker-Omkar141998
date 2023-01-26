906dc006c456a7ba2cf815cb44c08bd524015925
Omkar141998 <120208397+Omkar141998@users.noreply.github.com>
2023-01-26 11:30:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-// // complete the given function
-
-// function palindrome(str){
-// 	const alphanumericOnly = str.toLowerCase().match(/[a-z0-9]/g);
-// // non alpha numric character
-// 	return aplphanumricOnly.join(") === alphanumricOnly.reverse().join(");
-
-// }
-// coonsole.log(palindrome("racecar"));
-// module.exports = palindrome
 // complete the given function
 
 function palindrome(str){
-    const alphanumericOnly = str.toLowerCase().match(/[a-z0-9]/g);
-    // 2) Strip out non-alphanumeric characters
-    
-// 3) return string === reversedString
-return alphanumericOnly.join('') === alphanumericOnly.reverse().join('');
-    
-}
-console.log(palindrome("racecar"));
+	const alphanumericOnly = str.toLowerCase().match(/[a-z0-9]/g);
+// non alpha numric character
+	return aplphanumricOnly.join('') === alphanumricOnly.reverse().join('');
 
+}
+coonsole.log(palindrome("racecar"));
 module.exports = palindrome
+// // complete the given function
+
+// function palindrome(str){
+//     const alphanumericOnly = str.toLowerCase().match(/[a-z0-9]/g);
+//     // 2) Strip out non-alphanumeric characters
+    
+// // 3) return string === reversedString
+// return alphanumericOnly.join('') === alphanumericOnly.reverse().join('');
+    
+// }
+// console.log(palindrome("racecar"));
+
+// module.exports = palindrome
